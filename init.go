5a321f0811d4289fe73d231136b4c77a3aaf95aa
Luis Enriquez Aracena Cabrera <l@luise.ac>
2025-06-11 18:42:18 +0200

    Feat: added delete feature with param --delete

--- init.go
+++ init.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"fmt"
-
+	"bufio"
 	"flag"
+	"fmt"
+	"os"
 	"strconv"
+	"strings"
 	"unicode"
 
 	"github.com/IceWizard98/series_downloader/models"
 	"github.com/IceWizard98/series_downloader/models/animeunity"
 	"github.com/IceWizard98/series_downloader/models/user"
-	"github.com/IceWizard98/series_downloader/utils/bloomFilter"
+	bloomfilter "github.com/IceWizard98/series_downloader/utils/bloomFilter"
 	"github.com/IceWizard98/series_downloader/utils/routinepoll"
 	"github.com/skratchdot/open-golang/open"
-
-	"bufio"
-	"os"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-
 func main() {
 	serie_title := flag.String("title", "", "Anime title")
-	userName    := flag.String("user", "", "Eser.env file for configuration loading")
+	userName := flag.String("user", "", "Eser.env file for configuration loading")
+	delete_prev := flag.Bool("delete", false, "Delete previus episodes")
 
 	flag.Parse()
 
@@ -38,7 +36,7 @@ func main() {
 		return
 	}
 
-  userRootDir := os.Getenv("USER_ROOT_DIR")
+	userRootDir := os.Getenv("USER_ROOT_DIR")
 
 	if userRootDir == "" {
 		fmt.Println("USER_ROOT_DIR is not set")
@@ -46,7 +44,7 @@ func main() {
 	}
 
 	filter := bloomfilter.GetInstance()
-	user   := user.GetInstance(*userName, userRootDir)
+	user := user.GetInstance(*userName, userRootDir)
 
 	fmt.Println(filter.Filter)
 	if *serie_title == "" || len(*serie_title) == 0 {
@@ -55,7 +53,7 @@ func main() {
 	}
 
 	animeUnityInstance := animeunity.Init()
-	animeList          := animeUnityInstance.Search(*serie_title)
+	animeList := animeUnityInstance.Search(*serie_title)
 
 	if len(animeList) == 0 {
 		fmt.Println("No results found")
@@ -63,14 +61,14 @@ func main() {
 	}
 
 	for i, v := range animeList {
-		fmt.Printf("%d - %s\n", i + 1, v.Slug)
+		fmt.Printf("%d - %s\n", i+1, v.Slug)
 	}
 
 	fmt.Println("Select anime")
 	reader := bufio.NewReader(os.Stdin)
 
-  selected, _ := reader.ReadString('\n') 
-	selected     = strings.TrimSpace(selected)
+	selected, _ := reader.ReadString('\n')
+	selected = strings.TrimSpace(selected)
 
 	if selected == "" || len(selected) == 0 {
 		fmt.Println("Invalid selection")
@@ -84,7 +82,8 @@ func main() {
 		}
 	}
 
-	index_selected, err := strconv.ParseUint(selected, 10, 16); if err != nil {
+	index_selected, err := strconv.ParseUint(selected, 10, 16)
+	if err != nil {
 		panic(err)
 	}
 
@@ -94,21 +93,21 @@ func main() {
 	}
 
 	var selectedEpisode models.Episode
-	selectedAnime := animeList[index_selected - 1]
-  toContinue    := false
+	selectedAnime := animeList[index_selected-1]
+	toContinue    := false
 	for _, v := range user.GetHistory() {
 		if v.AnimeID == selectedAnime.ID {
 			fmt.Printf("Current episode: %d\n", v.EpisodeNumber)
 			fmt.Println("Do you want to whatch the next episode? (y/n)")
 			reader := bufio.NewReader(os.Stdin)
 
-			to_continue, _ := reader.ReadString('\n') 
-			to_continue     = strings.TrimSpace(to_continue)
-			to_continue     = strings.ToLower(to_continue)
+			to_continue, _ := reader.ReadString('\n')
+			to_continue = strings.TrimSpace(to_continue)
+			to_continue = strings.ToLower(to_continue)
 
 			if to_continue == "y" {
 				selectedEpisode = models.Episode{
-					ID: v.EpisodeID,
+					ID:     v.EpisodeID,
 					Number: v.EpisodeNumber,
 				}
 				toContinue = true
@@ -118,23 +117,29 @@ func main() {
 
 	var episodes []models.Episode
 	if toContinue {
-  	episodes = animeUnityInstance.GetEpisodes(selectedAnime)
+		episodes = animeUnityInstance.GetEpisodes(selectedAnime)
 
-	  if uint16(len(episodes)) <= selectedEpisode.Number {
-      fmt.Println("Anime is over, well done!")
+		if uint16(len(episodes)) <= selectedEpisode.Number {
+			fmt.Println("Anime is over, well done!")
 			return
 		}
 
-    fmt.Printf("Continue watching episode %d\n", selectedEpisode.Number+1)
-  	selectedEpisode = episodes[selectedEpisode.Number]
+		fmt.Printf("Continue watching episode %d\n", selectedEpisode.Number+1)
+		selectedEpisode = episodes[selectedEpisode.Number]
 	} else {
-  	episodes = animeUnityInstance.GetEpisodes(selectedAnime)
-  	for i, v := range episodes {
-  		fmt.Printf("%d - %d\n", i + 1, v.Number)
-  	}
-  
-    selected, _ = reader.ReadString('\n') 
-  	selected    = strings.TrimSpace(selected)
+		episodes = animeUnityInstance.GetEpisodes(selectedAnime)
+
+		if len(episodes) == 0 {
+			fmt.Println("No episodes found")
+			return
+		}
+
+		for i, v := range episodes {
+			fmt.Printf("%d - %d\n", i+1, v.Number)
+		}
+
+		selected, _ = reader.ReadString('\n')
+		selected = strings.TrimSpace(selected)
 
 		if selected == "" || len(selected) == 0 {
 			fmt.Println("Invalid selection")
@@ -147,41 +152,40 @@ func main() {
 				return
 			}
 		}
-  
-  	index_selected, err = strconv.ParseUint(selected, 10, 16)
-  	if err != nil {
-  		panic(err)
-  	}
-  	
-  	if index_selected < 1 || uint16(index_selected) > uint16(len(episodes)) {
-  		fmt.Println("Invalid selection")
-  		return
-  	}
-  
-  	selectedEpisode = episodes[index_selected - 1]
+
+		index_selected, err = strconv.ParseUint(selected, 10, 16)
+		if err != nil {
+			panic(err)
+		}
+
+		if index_selected < 1 || uint16(index_selected) > uint16(len(episodes)) {
+			fmt.Println("Invalid selection")
+			return
+		}
+
+		selectedEpisode = episodes[index_selected-1]
 	}
 
 	pool := routinepoll.GetInstance()
 
-	pool.AddTask( func() {
-	  func( episode models.Episode ) {
-	  	fmt.Printf("Downloading main episode %d\n", episode.Number)
-    	path, error := animeUnityInstance.DownloadEpisode(episode, user.RootDir)
+	pool.AddTask(func() {
+		func(episode models.Episode) {
+			// fmt.Printf("Downloading main episode %d\n", episode.Number)
+			path, error := animeUnityInstance.DownloadEpisode(episode, user.RootDir)
 
-	  	if error != nil {
-	  		fmt.Printf("Error downloading episode %d: %s\n", episode.Number, error)
-	  	} else {
-	  		fmt.Printf("Episode downloaded: %d\n", episode.Number)
-	  		user.AddHistory( "animeunity", selectedAnime.ID, episode ) 
+			if error != nil {
+				fmt.Printf("Error downloading episode %d: %s\n", episode.Number, error)
+			} else {
+				fmt.Printf("Episode downloaded: %d\n", episode.Number)
+				user.AddHistory("animeunity", selectedAnime.ID, episode)
 				if err := open.Run(path); err != nil {
 					fmt.Printf("Error opening file to Play episode %s: %s\n", path, err)
 				}
-	  	}
-	  }( selectedEpisode )
+			}
+		}(selectedEpisode)
 	})
 
-
-  downloadNextNEpisodes := os.Getenv("DOWNLOAD_NEXT_EPISODES")
+	downloadNextNEpisodes := os.Getenv("DOWNLOAD_NEXT_EPISODES")
 
 	if downloadNextNEpisodes == "" || len(downloadNextNEpisodes) == 0 {
 		pool.Wait()
@@ -196,19 +200,20 @@ func main() {
 		}
 	}
 
-	nextNEpisodes, err := strconv.ParseUint(downloadNextNEpisodes, 10, 16); if err != nil {
+	nextNEpisodes, err := strconv.ParseUint(downloadNextNEpisodes, 10, 16)
+	if err != nil {
 		fmt.Printf("Error parsing %s: %s\n", downloadNextNEpisodes, err)
 	}
 
-	fmt.Printf("Downloading %d next episodes\n", nextNEpisodes)
+	// fmt.Printf("Downloading %d next episodes\n", nextNEpisodes)
 	// The iterator starts at 0, but the first episode has number = 1 and is at index 0 in the slice.
 	// This means we can simply add the iterator to the number of episodes already downloaded —
 	// the result will correctly match the index in the slice.
 	for iterator := range uint16(nextNEpisodes) {
-  	if uint16(len(episodes)) > selectedEpisode.Number + iterator {
-			pool.AddTask( func() {
-				episode := episodes[selectedEpisode.Number + iterator]
-				fmt.Printf("Downloading episode %d\n", episode.Number)
+		if uint16(len(episodes)) > selectedEpisode.Number+iterator {
+			pool.AddTask(func() {
+				episode := episodes[selectedEpisode.Number+iterator]
+				// fmt.Printf("Downloading episode %d\n", episode.Number)
 
 				_, error := animeUnityInstance.DownloadEpisode(episode, user.RootDir)
 
@@ -218,11 +223,41 @@ func main() {
 					fmt.Printf("Episode downloaded: %d\n", episode.Number)
 				}
 			})
-  	} else {
+		} else {
 			break
 		}
 	}
 
+	if *delete_prev {
+		basePath := fmt.Sprintf(user.RootDir+"/%s", selectedAnime.Slug)
+		files, err := os.ReadDir(basePath)
+		if err != nil {
+			fmt.Printf("Error reading directory %s: %s\n", basePath, err)
+		}
+
+		for _, file := range files {
+			pool.AddTask(func() {
+				func(file os.DirEntry) {
+					episodeNumber, err := strconv.Atoi(strings.Split(file.Name(), ".")[0])
+					if err != nil {
+						fmt.Printf("Error parsing file name %s: %s\n", file.Name(), err)
+						return
+					}
+
+					if episodeNumber >= int(selectedEpisode.Number) {
+						return
+					}
+
+					fmt.Printf("Deleting file %s\n", basePath+"/"+file.Name())
+
+					if err := os.Remove(basePath + "/" + file.Name()); err != nil {
+						fmt.Printf("Error deleting file %s: %s\n", basePath+"/"+file.Name(), err)
+					}
+				}(file)
+			})
+		}
+	}
+
 	pool.Wait()
-	//TODO: currently useless but filter must be updated on --serve version
+	// TODO: currently useless but filter must be updated on --serve version
 }
