6ef887a1012648d797f8f8c86d61969703ec8a9c
Luis Enriquez Aracena Cabrera <l@luise.ac>
2025-07-08 09:52:05 +0200

    Fix: resolve race condition in bloomfilter instance

--- utils/bloomFilter/BloomFilter.go
+++ utils/bloomFilter/BloomFilter.go
@@ -1,23 +1,28 @@
 package bloomfilter
 
 import (
+	"sync"
 	"github.com/cespare/xxhash/v2"
 )
 
-var instance *bloomFilter
-type bloomFilter struct{
+var (
+	instance *bloomFilter
+	once     sync.Once
+)
+
+type bloomFilter struct {
 	k      uint32
 	Filter uint32
+	mutex  sync.RWMutex
 }
 
 func GetInstance() *bloomFilter {
-	if instance == nil {
+	once.Do(func() {
 		instance = &bloomFilter{
 			k:      3,
 			Filter: 0,
 		}
-	}
-
+	})
 	return instance
 }
 
@@ -26,17 +31,20 @@ func (b *bloomFilter) getHashes(value []byte) []uint32 {
 	hash   := xxhash.Sum64(value)
 	h1     := uint32(hash & 0xFFFFFFFF)
 	h2     := uint32((hash >> 32) & 0xFFFFFFFF)
-
-	for i := range b.k {
+	
+	for i := uint32(0); i < b.k; i++ {
 		generated := h1 + h2*i
-		hashes[i]  = uint32(generated % 32)
+		hashes[i] = generated % 32
 	}
-
 	return hashes
 }
 
 func (b *bloomFilter) Add(value []byte) {
 	hashes := b.getHashes(value)
+	
+	b.mutex.Lock()
+	defer b.mutex.Unlock()
+	
 	for _, hash := range hashes {
 		b.Filter |= 1 << hash
 	}
@@ -44,6 +52,10 @@ func (b *bloomFilter) Add(value []byte) {
 
 func (b *bloomFilter) Contains(value []byte) bool {
 	hashes := b.getHashes(value)
+	
+	b.mutex.RLock() 
+	defer b.mutex.RUnlock()
+	
 	for _, hash := range hashes {
 		if (b.Filter & (1 << hash)) == 0 {
 			return false
